dd732951f5ff2570b7ab02a5d46f68348b22f1c3
Aylén Orellana <ayluorellana@gmail.com>
2024-11-03 12:35:46 +0100

    exercise 2: adding draft 2

--- battleHorde.js
+++ battleHorde.js
@@ -6,10 +6,17 @@ function battleHorde(zombies, humans) {
     if (zombies[i] < humans[i]) {
       let points = humans[i] - zombies[i];
       if (i === 2) {
-        return `${points}h`;
+        return points ? `${points}h` : "x";
+      }
+    }
+    if (humans[i] < zombies[i]) {
+      let points = zombies[i] - humans[i];
+      if (i === 2) {
+        return points ? `${points}z` : "x";
       }
     }
   }
-
   return "x";
 }
+
+console.log(battleHorde("444", "282"));
